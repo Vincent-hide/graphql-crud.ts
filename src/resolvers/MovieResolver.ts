0481f876e908392b02326ac3bf746b4238f6411d
Vincent-hide <hidehiro.okayama@gmail.com>
2020-12-28 13:43:25 -0500

    input type created and update mutation added

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -1,17 +1,35 @@
-import {Arg, Mutation, Resolver, Int, Query} from "type-graphql";
+import {Arg, Mutation, Resolver, Int, Query, InputType, Field} from "type-graphql";
 import {Movie} from "../entity/Movie";
 
-// @ObjectType()
+@InputType()
+class MovieInput {
+  @Field()
+  title: string;
+
+  @Field(() => Int)
+  minutes: number;
+}
+
+@InputType()
+class MovieUpdate {
+  @Field(() => String, {nullable: true})
+  title?: string;
+
+  @Field(() => Int, {nullable: true})
+  minutes?: number;
+}
+
 @Resolver()
 export class MovieResolver {
+  @Mutation(() => Movie)
+  async createMovie(@Arg("options", () => MovieInput) options: MovieInput) {
+    const movie = await Movie.create(options).save();
+    return movie;
+  }
+
   @Mutation(() => Boolean)
-  async createMovie(
-    @Arg('title', () => String, {nullable: true}) title: string | null,
-    @Arg('minutes', () => Int) minutes: number
-  ) {
-    console.log("args", title, minutes);
-    // @ts-ignore
-    await Movie.insert({title, minutes});
+  async updateMovie(@Arg('id') id: number, @Arg('input', () => MovieUpdate) input: MovieUpdate) {
+    await Movie.update({id}, input);
     return true;
   }
 
